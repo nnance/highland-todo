96f8553c3a07d39cf42bf9a1b33509dbe50d435c
Nick Nance <nance.nick@gmail.com>
2016-03-12 08:22:54 -0800

    added sortByDueDate

--- index.js
+++ index.js
@@ -2,13 +2,30 @@
 
 const _ = require("highland")
 
-const incomplete = function (obj) {
-  return obj.complete === false
+function filterComplete(stream) {
+  return stream.filter((obj) => obj.complete === false)
 }
 
-const filterToDos = _.pipeline((s) => {
-  return s.filter(incomplete)
-})
+function sortByDueDate(stream) {
+
+  return stream.sortBy(_.flip(sortDate))
+
+  function sortDate(a, b) {
+    if (a.dueDate < b.dueDate) {
+      return -1
+    } else if (a.dueDate > b.dueDate) {
+      return 1
+    } else {
+      return 0
+    }
+  }
+
+}
+
+const filterToDos = _.pipeline(
+  filterComplete,
+  sortByDueDate
+)
 
 module.exports = {
   filterToDos
--- test/todo.js
+++ test/todo.js
@@ -8,25 +8,35 @@ const api = require("../index")
 const data = [
   {
     subject: "Read about functional programming",
+    dueDate: "01/01/2016",
     complete: false
   },
   {
     subject: "Try out the Node Stream Adventure",
+    dueDate: "02/01/2016",
     complete: false
   },
   {
     subject: "Build Todo app based on Highland js",
+    dueDate: "01/15/2016",
     complete: true
   }
 ]
 
 describe("Functional Todo app", () => {
   describe("When filtering incomplete items", () => {
-    it("should return 2 items", (done) => {
+    let results
+    before((done) => {
       _(data).pipe(api.filterToDos).toArray((x) => {
-        expect(x.length).to.equal(2)
+        results = x
         done()
       })
+    })
+    it("should return 2 items", () => {
+      expect(results.length).to.equal(2)
+    })
+    it("should be sorted in descending order", () => {
+      expect(results[0].dueDate).to.equal("02/01/2016")
     })
   })
 })
